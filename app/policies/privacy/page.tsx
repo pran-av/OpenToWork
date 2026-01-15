aafd9a64798400f722f0f0c9a7dca041df79bbf6
Pranav M <pranavmandhare27@gmail.com>
2026-01-15 17:14:08 +0530

    update: privacy policy v0.2.0

--- app/policies/privacy/page.tsx
+++ app/policies/privacy/page.tsx
@@ -6,7 +6,7 @@ import PolicyHeader from "../PolicyHeader";
 
 async function getPrivacyPolicyContent() {
   try {
-    const filePath = join(process.cwd(), "policies", "privacy", "v0.1.0-2026-01-07.md");
+    const filePath = join(process.cwd(), "policies", "privacy", "v0.2.0-2026-01-15.md");
     const fileContents = await readFile(filePath, "utf8");
     return fileContents;
   } catch (error) {
